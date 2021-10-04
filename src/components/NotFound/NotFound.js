999b7f4fbce90fd749d98f92a4c55df05c156273
evanmozumder <evanmozumder5@gmail.com>
2021-10-04 18:36:16 +0600

    not found edit

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,7 +4,16 @@ const NotFound = () => {
   // handling 404 status
   return (
     <div>
-      <h2 className="text-4xl">404 not found!!</h2>
+      <div
+        style={{
+          height: "70vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h2 className="text-6xl ">404 not found!!</h2>
+      </div>
     </div>
   );
 };
